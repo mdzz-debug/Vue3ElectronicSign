02dc9290589ac03ece0fdde48a211e364cc0f840
罗昊 <1356060370@qq.com>
2024-02-04 18:00:43 +0800

    refactor:

--- examples/packages/index.ts
+++ examples/packages/index.ts
@@ -1,10 +1,12 @@
-// index.ts
-import type { App } from 'vue'
-import ElectronicSign from "./electronic_sign/index.vue";
+//package/index.js
+import Electronic_sign from "./electronic_sign/electronic_sign.vue";
+const coms = [Electronic_sign]; // 将来如果有其它组件,都可以写到这个数组里
 
-// 使用install方法，在app.use挂载
-ElectronicSign.install = (app: App) => {
-  app.component(ElectronicSign.__name as string, ElectronicSign) //注册组件
-}
+// 批量组件注册
+const install = function (Vue: any) {
+  coms.forEach((com) => {
+    Vue.component(com.name, com);
+  });
+};
 
-export default ElectronicSign
+export default install;
